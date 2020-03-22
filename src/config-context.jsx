70dfbd5c1387dbbb53e071cd0c779ec4c9039abd
Calin Tamas <calinntamas@gmail.com>
2020-03-22 16:07:45 +0200

    update config context

---
+++ src/config-context.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import defaultConfig from './config';
+import { merge } from './utils/object';
+
+const ConfigContext = React.createContext({});
+const ConfigConsumer = ConfigContext.Consumer;
+
+const ConfigProvider = ({ children, config: customConfig }) => {
+  const [config, setConfig] = useState(merge(defaultConfig, customConfig));
+
+  const setTheme = (newTheme = '') => {
+    setConfig({
+      ...config,
+      activeTheme: newTheme
+    });
+  };
+
+  const { activeTheme } = config;
+  const value = {
+    theme: config[activeTheme],
+    setTheme
+  };
+
+  return (
+    <ConfigContext.Provider value={value}>{children}</ConfigContext.Provider>
+  );
+};
+
+export { ConfigProvider, ConfigConsumer };
+export default ConfigContext;
--- src/config.js
+++ src/config.js
@@ -1,62 +1,41 @@
-import { merge } from './utils/object';
-// eslint-disable-next-line
-// import customConfig from '../../primitives.config.js'; // TODO: put this on more level deep
-
-const customConfig = {};
-
-const config = {
-  typography: {
-    fontFamily: {
-      default: 'Helvetica'
-    },
-    fontWeight: {
-      extralight: '100',
-      thin: '200',
-      light: '300',
-      normal: '400',
-      medium: '500',
-      semibold: '600',
-      bold: '700',
-      heavy: '800',
-      black: '900'
-    },
-    fontSize: {
-      h6: 12,
-      h5: 14,
-      h4: 16,
-      h3: 20,
-      h2: 24,
-      h1: 32
-    }
-  },
-
-  spacer: {
-    smallest: 6,
-    small: 10,
-    medium: 14,
-    default: 18,
-    large: 22,
-    largest: 26
-  },
-
-  scheme: 'default',
-
-  colors: {
-    default: {
-      brand: '#28D9AA',
-      background: '#FFF',
-      text: {
-        default: '#000'
-      }
-    },
-    dark: {
-      brand: '#28D9AA',
-      background: '#000',
-      text: {
-        default: '#FFF'
-      }
-    }
-  }
+const weights = {
+  extralight: '100',
+  thin: '200',
+  light: '300',
+  normal: '400',
+  medium: '500',
+  semibold: '600',
+  bold: '700',
+  heavy: '800',
+  black: '900'
 };
 
-export default merge(config, customConfig);
+const defaultTheme = {
+  fontSize: [32, 24, 20, 16, 14, 12, 10, 8],
+  fontWeight: 'normal',
+  fontFamily: undefined,
+  weights,
+  spacer: [26, 22, 18, 14, 10, 6],
+  colors: {
+    brand: '#28D9AA',
+    background: '#FFF',
+    text: '#000'
+  },
+  isDark: false
+}
+
+const darkTheme = {
+  ...defaultTheme,
+  colors: {
+    brand: '#28D9AA',
+    background: '#000',
+    text: '#FFF'
+  },
+  isDark: true
+}
+
+export default {
+  activeTheme: 'default',
+  default: defaultTheme,
+  dark: darkTheme
+}
