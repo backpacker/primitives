3fe96664d6c8182180417615ff3c891e50cb4195
Calin Tamas <calinntamas@gmail.com>
2020-03-22 13:01:45 +0200

    use husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   extends: ['airbnb', 'prettier'],
   plugins: ['react-hooks'],
   globals: {
@@ -9,17 +8,13 @@ module.exports = {
   },
   rules: {
     'react/prop-types': 0,
-    'react/jsx-filename-extension': 0,
-    'react/jsx-closing-bracket-location': 0,
     'react/jsx-props-no-spreading': 0,
     'react/no-array-index-key': 0,
-    'react/sort-comp': 0,
     'react-hooks/rules-of-hooks': 2,
     'react-hooks/exhaustive-deps': 1,
-    'prefer-const': 0,
     'no-underscore-dangle': 0,
-    'no-plusplus': 0,
+    'import/no-extraneous-dependencies': 0,
     'import/prefer-default-export': 0,
-    'import/no-extraneous-dependencies': 0
+    'react/jsx-closing-bracket-location': 0
   }
 };
