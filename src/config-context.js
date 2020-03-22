7cddfc687a709bfb43d50ae94a589a27f8d37903
Calin Tamas <calinntamas@gmail.com>
2020-03-22 16:07:45 +0200

    update config context

--- src/config-context.js
+++ src/config-context.js
@@ -1,49 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react'
 
-import defaultConfig from './config';
-import { merge } from './utils/object';
+import defaultConfig from './config'
+import { merge } from './utils/object'
 
-const ConfigContext = React.createContext();
+const ConfigContext = React.createContext({});
 const ConfigConsumer = ConfigContext.Consumer;
 
-export default class ConfigProvider extends Component {
-  constructor(props) {
-    super(props);
-    this.updateConfig = this.updateConfig.bind(this);
+const ConfigProvider = ({ children, config: customConfig }) => {
+  const [config, setConfig] = useState(merge(defaultConfig, customConfig))
 
-    this.state = {
-      config: defaultConfig
-    };
+  const setTheme = (newTheme = '') => {
+    setConfig({
+      ...config,
+      activeTheme: newTheme
+    })
   }
 
-  updateConfig(newConfig = {}) {
-    this.setState((state) => {
-      const newState = {
-        ...state,
-        config: merge(state.config, newConfig)
-      };
-      return newState;
-    });
+  const { activeTheme } = config;
+  const value = {
+    theme: config[activeTheme],
+    setTheme
   }
 
-  render() {
-    const { children } = this.props;
-    const { config } = this.state;
-
-    const { colors, scheme } = config;
-
-    const value = {
-      config: {
-        ...config,
-        colorScheme: colors[scheme]
-      },
-      updateConfig: this.updateConfig
-    };
-
-    return (
-      <ConfigContext.Provider value={value}>{children}</ConfigContext.Provider>
-    );
-  }
+  return (
+    <ConfigContext.Provider value={value}>
+      {children}
+    </ConfigContext.Provider>
+  )
 }
 
-export { ConfigProvider, ConfigConsumer };
+export { ConfigProvider, ConfigConsumer }
+export default ConfigContext
